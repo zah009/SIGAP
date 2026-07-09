79f556a61bfc00ab212eb1d4456bd5c73994f591
zah009 <saidhamzah12@gmail.com>
2026-07-09 12:22:31 +0700

    feat: database seeders for testing

---
+++ app/Database/Seeds/RegularUserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class RegularUserSeeder extends Seeder
+{
+    public function run()
+    {
+        $data = [
+            'username'      => 'user1',
+            'password_hash' => password_hash('user123', PASSWORD_DEFAULT),
+            'role'          => 'user',
+            'nama_lengkap'  => 'User Divisi Keuangan',
+            'created_at'    => date('Y-m-d H:i:s'),
+        ];
+
+        $this->db->table('users')->insert($data);
+    }
+}
